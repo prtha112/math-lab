98e9795486c4a86ca24bc80935b10c162cdf9ad5
Sathabhron Changchuea <sathabhronchangchuea@Sathabhrons-MacBook-Air.local>
2021-07-18 15:03:11 +0700

    Add set labs example.

---
+++ set_difference_example1.py
@@ -0,0 +1,17 @@
+# Student 100 peple.
+# 23 Students like to play sport.
+# 28 Students like lisntening to music.
+# 12 Students don't like anything.
+
+totalStudents = 100
+
+studentSport = 23
+studentSong = 28
+studentNone = 12
+student = None
+
+totalKnow = studentSport + studentSong + studentNone
+print(totalKnow)
+
+totalStudentLikeSportSong = totalStudents - totalKnow
+print(totalStudentLikeSportSong) # This is students like sport and music.
