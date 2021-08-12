b83b6f7317d94238c575c0093450b32e49119d60
Sathabhron Changchuea <sathabhronchangchuea@Sathabhrons-MacBook-Air.local>
2021-08-12 21:49:26 +0700

    trigonmetric

---
+++ trigonometric.py
@@ -0,0 +1,11 @@
+import math
+
+print("Hello world")
+print(math.radians(90)) # from radians to degrees.
+print(math.degrees(1.570796)) # from degrees to radians.
+print(math.degrees((math.pi/2))) # from degrees to radians.
+
+print(math.radians(90))
+print(math.radians(180))
+print(math.radians(270))
+print(math.radians(360))
