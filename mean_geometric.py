c04223adcb8fc75ad6dcf49dc693596e695d159d
Sathabhron Changchuea <sathabhronchangchuea@Sathabhrons-MacBook-Air.local>
2021-08-12 23:37:15 +0700

    mean geometric.

---
+++ mean_geometric.py
@@ -0,0 +1,8 @@
+import math
+# Example: What is the Geometric Mean of 10, 51.2 and 8?
+
+multiply = 10 * 51.2 * 8
+root3 =  multiply**(1/3) # หารูท 3
+
+print("Geometric Mean = 3√(10 × 51.2 × 8) = {0}".format(round(root3)))
+print("Geometric Mean = 10 * 51.2 * 8 = 16 * 16 * 16")
